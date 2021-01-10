3a384da6dc13b6379c012485cb37497f593f8ced
jonggulee <jg.jake.lee@gmail.com>
2021-01-10 03:59:47 +0000

    Update IAM 20210110

--- IAM/devops_group.tf
+++ IAM/devops_group.tf
@@ -1,3 +1,13 @@
 resource "aws_iam_group" "devops_group" {
 	name = "devops"
 }
+
+resource "aws_iam_group_membership" "devops" {
+	name = aws_iam_group.devops_group.name
+
+	users = [
+		aws_iam_user.infra_test.name
+	]
+
+	group = aws_iam_group.devops_group.name
+}
