77db25249f3cf281aa2975ae6c346eb35c9fad71
jonggulee <jg.jake.lee@gmail.com>
2021-01-31 09:48:14 +0000

    Create IAM Role

---
+++ IAM/iam_role_hello.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "hello" {
+  name               = "hello-iam-role"
+  path               = "/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy" "hello_s3" {
+  name   = "hello-s3-download"
+  role   = aws_iam_role.hello.id
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Sid": "AllowAppArtifactsReadAccess",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "*"
+      ],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_instance_profile" "hello" {
+  name = "hello-profile"
+  role = aws_iam_role.hello.name
+}
+
